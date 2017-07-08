a967919ebd6e8549213020792aefc45b083e1ac0
Fred Block <info@fredblock.com>
2017-07-08 15:48:31 -0500

    Update header and footer templates Header: add dynamic page title. Footer: add dynamic name + date.

--- wp-content/themes/wordpress-bootstrap/footer.php
+++ wp-content/themes/wordpress-bootstrap/footer.php
@@ -1,10 +1,11 @@
       <hr>
 
       <footer>
-        <p>&copy; 2016 Company, Inc.</p>
+        <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?></p>
       </footer>
 
-    </div> <!-- /container -->
+    </div>
+    <!-- /container -->
 
     <?php wp_footer(); ?>
 
